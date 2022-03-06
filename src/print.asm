a01699897198f2f349dc8afa3e09aa862b5c1113
Malcret <t.marcopoulos@outlook.fr>
2022-03-06 19:59:57 +0100

    update comments

--- src/print.asm
+++ src/print.asm
@@ -1,22 +1,11 @@
 [bits 16]
 
-clear_screen:
-	mov ah, 0x07 ; scroll down screen
-	mov al, 0x00 ; scroll whole screen
-	mov bh, 0x0f ; set color to white on black
-	mov ch, 0x00 ; upper row
-	mov cl, 0x00 ; left column
-	mov dh, 0x18 ; lower row
-	mov dl, 0x4f ; right column
-	int 0x10 ; clear screen
-
-	ret
-
 print_string:
-	lodsb ; load msg first byte from SI
+	lodsb ; load to al next msg byte from si
 
-	or al, al ; check if not 0
-	jz .exit ; if 0 exit
+	; check if reach the of end of the msg, if true exit
+	or al, al
+	jz .exit
 
 	mov ah, 0x0e ; write character mode
 	int 0x10 ; print character
